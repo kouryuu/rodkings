387eb1c10f770d3a044926952a9be699d2e5906e
kouryuu <antarticfreeze@gmail.com>
2015-07-30 17:39:05 -0500

    Ditching auto generate JSON for later

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -2,5 +2,5 @@ var myApp = angular.module('Blog',[]);
 
 myApp.controller('mainContent',['$scope',function($scope){
   $scope.title = 'Popular Posts';
-
+  
 }]);
--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,10 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var jade = require('gulp-jade');
 var markdown = require('gulp-markdown');
-// FS magic
-var fs = require('fs');
-var sys = require('sys');
-var exec = require('child_process').exec;
-var colors = require('colors');
-var child;
+
 var tags = require('./tags.json');
 // Function that cleans the names to put in the JSON file
-var cleanName = (function(dirty){
-  clean = dirty.split('/');
-  clean = clean[clean.length - 1];
-  clean = clean.split('.md');
-  clean = clean[0];
-  return clean;
-});
+
 
 gulp.task('compileJade', function() {
   console.log('compiling..');
@@ -32,35 +21,7 @@ gulp.task('compileMarkdown',function(){
   .pipe(markdown())
   .pipe(gulp.dest('build/posts/'));
 })
-gulp.task('generateJSON',function() {
-  post_tags = tags.tags;
-  json = '{';
-  posts_left = post_tags.length;
 
-  // for(i=0;i < post_tags.length ; i ++){
-    console.log('ls -1 ./'+post_tags[0]+'/*.md');
-  child = exec('ls -1 ./UX/*.md',function(error,stdout,stderr){
-    console.log('i am here');
-    var posts = Array();
-     posts = stdout.split('\n');
-     posts.pop();
-     posts = posts.map(cleanName);
-     posts_left = posts_left - 1;
-     return JSON.stringify(post_tags[i])+':' + JSON.stringify(posts);
-  });
-  // }
-  while(posts_left > 0){
-    //console.log(posts_left);
-
-  }
-  json += JSON.stringify(child) + '}';
-  fs.writeFile('posts.json', json, function (err) {
-    if (err) return console.log(err.red);
-    console.log('[Wrote] to '.green +'posts.json'.cyan);
-  });
-  gulp.src('posts.json')
-  .pipe(gulp.dest('build/ajax/'));
-});
 gulp.task('compress',function() {
   console.log('compressing.');
   gulp.src('scripts/*.js')
