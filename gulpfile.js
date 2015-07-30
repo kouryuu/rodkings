2de56b418258379faca055f60281fe1ed2482b8a
kouryuu <antarticfreeze@gmail.com>
2015-07-30 02:48:12 -0500

    Use Angular minimaly

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 var gulp = require('gulp');
 // Live reaload
 
+
+
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var jade = require('gulp-jade');
+// FS magic
+var fs = require('fs');
+var sys = require('sys');
+var exec = require('child_process').exec;
+var colors = require('colors');
+var child;
+// Function that cleans the names to put in the JSON file
+var cleanName = (function(dirty){
+  clean = dirty.split('/');
+  clean = clean[clean.length - 1];
+  clean = clean.split('.md');
+  clean = clean[0];
+  return clean;
+});
+
 gulp.task('compileJade', function() {
   console.log('compiling..');
   gulp.src('templates/*.jade')
@@ -11,9 +28,27 @@ gulp.task('compileJade', function() {
   .pipe(gulp.dest('build/'));
 });
 
+
+gulp.task('generateJSON',function() {
+  child = exec('ls -1 ./*/*.md',function(error,stdout,stderr){
+    var posts = Array();
+     posts = stdout.split('\n');
+     posts.pop();
+     posts = posts.map(cleanName);
+     json = '{'+JSON.stringify(posts)+'}';
+
+     fs.writeFile('posts.json', json, function (err) {
+    if (err) return console.log(err.red);
+    console.log('[Wrote] to '.green +'posts.json'.cyan);
+    });
+
+  });
+});
 gulp.task('compress',function() {
   console.log('compressing.');
   gulp.src('scripts/*.js')
+  .pipe(gulp.dest('build/scripts/'));
+  gulp.src('scripts/*.map')
   .pipe(gulp.dest('build/scripts/'));
   gulp.src('styles/*.css')
   .pipe(gulp.dest('build/styles/'));
--- poster.js
+++ poster.js
@@ -2,10 +2,19 @@
 var fs = require('fs');
 var sys = require('sys');
 var exec = require('child_process').exec;
+var colors = require('colors');
 var child;
 
-child = exec('markdown UX/post1.md',function(error,stdout,stderr){
-  console.log(stdout);
+child = exec('ls -1 ./*/*.md',function(error,stdout,stderr){
+  var posts = Array();
+   posts = stdout.split('\n');
+   posts.pop();
+   json = '{'+JSON.stringify(posts)+'}';
+   fs.writeFile('posts.json', json, function (err) {
+  if (err) return console.log(err);
+  console.log('Wrote to posts.json'.yellow);
+  });
+
 });
 
 
