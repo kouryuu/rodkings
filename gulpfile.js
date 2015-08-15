67686da5e8f128a761bd92560d25c1c50278a290
kouryuu <antarticfreeze@gmail.com>
2015-08-14 22:40:38 -0500

    Updated node dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,21 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var jade = require('gulp-jade');
 var markdown = require('gulp-markdown');
+// FS magic
+var fs = require('fs');
+var sys = require('sys');
+var exec = require('child_process').exec;
+var colors = require('colors');
+var child;
 
-var tags = require('./tags.json');
 // Function that cleans the names to put in the JSON file
-
+var cleanName = (function(dirty){
+  clean = dirty.split('/');
+  clean = clean[clean.length - 1];
+  clean = clean.split('.md');
+  clean = clean[0];
+  return clean;
+});
 
 gulp.task('compileJade', function() {
   console.log('compiling..');
@@ -17,17 +28,21 @@ gulp.task('compileJade', function() {
 });
 
 gulp.task('compileMarkdown',function(){
-  gulp.src('./*/*.md')
+  gulp.src('./posts/*.md')
   .pipe(markdown())
   .pipe(gulp.dest('build/posts/'));
-})
 
+})
+gulp.task('generateJSON',function() {
+
+});
 gulp.task('compress',function() {
-  console.log('compressing.');
+  console.log('[compressing] '+'JS'.yellow);
   gulp.src('scripts/*.js')
   .pipe(gulp.dest('build/scripts/'));
   gulp.src('scripts/*.map')
   .pipe(gulp.dest('build/scripts/'));
+  console.log('[compressing] '+'Styles'.yellow);
   gulp.src('styles/*.css')
   .pipe(gulp.dest('build/styles/'));
 });
@@ -37,17 +52,13 @@ gulp.task('imgcopy',function() {
 });
 gulp.task('serve', function(){
   browserSync({
+    port: 8081,
     server: {
+
       baseDir: './build'
     }
   });
   gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: './build'}, ['compress',reload]);
   gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: '.'}, ['compress',reload]);
   gulp.watch(['*.jade'],{cwd:'./templates'},['compileJade','compress']);
-});
-
-gulp.task('default', function() {
-console.log(tags.tags);
-
-
 });
--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,5 @@ var myApp = angular.module('Blog',[]);
 myApp.controller('mainContent',['$scope',function($scope){
   $scope.title = 'Popular Posts';
   
+
 }]);
