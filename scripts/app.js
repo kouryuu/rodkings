10de21944cf898b24792033ebbed9ec178bcf327
kouryuu <antarticfreeze@gmail.com>
2016-02-09 00:16:05 -0600

    Added HashChange library to route simple paths

--- scripts/app.js
+++ scripts/app.js
@@ -3,10 +3,7 @@ var myApp = angular.module('Blog',[]);
 myApp.controller('mainContent',['$scope','$http','$log',function($scope,$http,$log){
   $scope.posts = [];
   $scope.error = false;
-  $http({
-    method: 'GET',
-    url: '/posts.json'
-  }).then(function successCallback(response) {
+  var drawPosts = function (all,tag,response){
     // Check the keys in the array and stores it in categories
     $scope.categories = [];
     Object.keys(response.data).forEach(
@@ -17,19 +14,68 @@ myApp.controller('mainContent',['$scope','$http','$log',function($scope,$http,$l
     );
     // Iterate over all posts and store them in posts
     $scope.posts = [];
-
-    for(i=0;i < $scope.categories.length; i++){
+    if(all){
+     cat_length = $scope.categories.length
+   }else{
+     cat_length = 1
+   }
+    for(i=0;i < cat_length; i++){
+      if(all){
       category = $scope.categories[i];
       postCategory = response.data[category];
+      }
+      else{
+        postCategory = response.data[tag];
 
+      }
       for(j=0;j < postCategory.length;j++){
+
         $scope.posts.push(postCategory[j]);
       }
     }
+    $log.log($scope.posts)
+  }
+  getPosts = function(){
+    $http({
+    method: 'GET',
+    url: '/posts.json'
+      }).then(function successCallback(response) {
+      $scope.postJSON = response;
+
 
     }, function errorCallback(response) {
+      $scope.postJSON = {};
       $scope.posts = [{"title":"Something went wrong while retrieving posts.","teaser":"Try refreshing your browser."}]
     });
+    }
+    drawPosts(true,null,$scope.postJSON);
+    $(window).hashchange( function(){
 
+      var tag = location.hash;
+      if(tag != "#UX"){
+        $log.log('Hola')
+        if($scope.postJSON != undefined && $scope.postJSON != {}){
+          drawPosts(false,'UX',$scope.postJSON);
+        }else{
+          $scope.posts = [{"title":"Something went wrong while retrieving posts.","teaser":"Try refreshing your browser."}]
+        }
+      }
+    });
 
 }]);
+
+
+// var route = crossroads.addRoute('#{tag}',function(tag){
+//   console.log(tag);
+// });
+// route.matched.add(console.log, console);
+
+$(function(){
+
+  // Bind the event.
+
+
+  // Trigger the event (useful on page load).
+  $(window).hashchange();
+
+});
